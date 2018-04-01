c8744369e20d6fefb571cc881b2e1fdbfd061780
akash15797 <30523475+akash15797@users.noreply.github.com>
2018-04-01 15:33:23 +0530

    Add files via upload

---
+++ AdminError.java
@@ -0,0 +1,17 @@
+package com.ims.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/AdminError")
+public class AdminError extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("adminloginform.html").include(request, response);
+	}
+
+}
